849a58617c140d32c6672ad9e42f4ff8434cdbf1
bamoke <bamoke@163.com>
2018-09-27 22:13:01 +0800

    modify zlc 09-27

--- zlc/Common/Conf/server.php
+++ zlc/Common/Conf/server.php
@@ -2,5 +2,6 @@
 return array(
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_USER'   => 'zlcsql', // 用户名
-    'DB_PWD'    => 'zlc20180911***', // 密码
+    'DB_PWD'    => '123456', // 密码
+    // 'DB_PWD'    => 'zlc20180911***', // 密码
 );

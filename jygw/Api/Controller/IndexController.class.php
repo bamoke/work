86f87fb0662e2c1b47bb3ef43be2650921fcf86a
wangyi <wetz1@163.com>
2019-05-13 08:07:07 +0800

    modify jygw api

--- jygw/Api/Controller/IndexController.class.php
+++ jygw/Api/Controller/IndexController.class.php
@@ -3,6 +3,43 @@ namespace Api\Controller;
 use Api\Common\Controller\BaseController;
 class IndexController extends BaseController {
     public function index(){
+        $bannerList = array(
+            array(
+                "id"    =>1,
+                "img"   =>"/static/images/banner-1.jpg",
+                "url"   =>""
+            )
+        );
+        $articleList = array(
+            array(
+                "id"    =>1,
+                "title" =>"框架的核心是一个响应的数据绑定系统，可以让数据与视图非常简单地保持同步",
+                "thumb" =>"/static/images/banner-1.jpg",
+                "cateName"  =>"今日珠海",
+                "date"  =>"2019-05-02",
+                "click" =>"834"
+            ),
+            array(
+                "id"    =>2,
+                "title" =>"视图层就会做相应的更新",
+                "thumb" =>"/static/images/banner-1.jpg",
+                "cateName"  =>"金湾头条",
+                "date"  =>"2019-05-02",
+                "click" =>"443"
+            )
+        );
+        $backData = array(
+            "code"  =>200,
+            "msg"   =>"success",
+            "data"  =>array(
+                "bannerList"      =>$bannerList,
+                "articleList"   =>$articleList,
+            )
+        );
+        $this->ajaxReturn($backData);
+
+    }
+    public function index_bf(){
         $isCollected = true;
         $isLike = true;
         $condition = array(
